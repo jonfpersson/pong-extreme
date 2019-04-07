28e1af5ef3c0623e33e5049a3d7ef1fc1fa11219
Jonathan Persson <jonfpersson@gmail.com>
2019-04-07 02:29:52 +0200

    Ini

---
+++ Assets/Scripts/ballMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballMovement : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    public int speed;
+	// Use this for initialization
+	void Start () {
+
+        int rand = Random.Range(0, 2);
+        if(rand == 1)
+        {
+            rb.velocity = new Vector2(-speed,0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(speed, 0);
+
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "blue paddle")
+        {
+            float dist = GameObject.Find("blue paddle").transform.position.y - transform.position.y;
+            Debug.Log("dist: " + dist);
+
+            if(dist > 0)
+            {
+                //Go Up
+            }
+            else if(dist == 0)
+            {
+                //Go Straight
+
+            }
+            else
+            {
+                //Go Down
+
+            }
+
+        }
+    }
+        
+
+    // Update is called once per frame
+    void Update () {
+		
+
+
+	}
+}
---
+++ Assets/Scripts/paddleControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddleControl : MonoBehaviour {
+
+    public Rigidbody2D rb1;
+    public Rigidbody2D rb2;
+    public int speed = 5;
+	
+    void FixedUpdate()
+    {
+        float blueMoveVertical = Input.GetAxisRaw("wasd");
+        Vector2 blueMovement = new Vector2(0, blueMoveVertical);
+        rb1.velocity = blueMovement * speed;
+
+        //Blue vs Red
+
+        float redMoveVertical = Input.GetAxisRaw("udlr");
+        Vector2 redMovement = new Vector2(0, redMoveVertical);
+        rb2.velocity = redMovement * speed;
+    }
+}
